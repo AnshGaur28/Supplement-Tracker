c46c98bda5e5397e242bd8bfc69a67af15479371
Ansh Gaur <ansh@tick2trade.com>
2025-08-09 23:32:33 +0530

    maskable icons

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,8 @@ const manifestForPWA = {
   background_color: "#ffffff",
   theme_color: "#1976d2",
   icons: [
-    { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-    { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png" }
+    { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png" , "purpose": "maskable" },
+    { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png" , "purpose": "maskable" }
   ],
 };
 
